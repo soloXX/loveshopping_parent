4fd93942df308b1edd56c744e45a4bd85e92c113
soloXX <1337301452@qq.com>
2019-01-11 01:13:32 +0800

    测试

--- plat_parent/plat_service_8001/src/main/java/org/pengxx/loveshopping/PlatApplication_8001.java
+++ plat_parent/plat_service_8001/src/main/java/org/pengxx/loveshopping/PlatApplication_8001.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * 平台服务入口类
+ */
 @SpringBootApplication
 @EnableEurekaClient
 public class PlatApplication_8001 {
